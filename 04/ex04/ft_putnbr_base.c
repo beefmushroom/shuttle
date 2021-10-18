20452ce6fb4257eb3eaa3500bfd6fd897e0c0ab6
Sungjun Kim <sungjuki@c6r6s7.42seoul.kr>
2021-10-18 10:27:45 +0900

    04

--- 04/ex04/ft_putnbr_base.c
+++ 04/ex04/ft_putnbr_base.c
@@ -6,67 +6,54 @@
 /*   By: sungjuki <sungjuki@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 09:29:36 by sungjuki          #+#    #+#             */
-/*   Updated: 2021/10/18 10:05:05 by sungjuki         ###   ########.fr       */
+/*   Updated: 2021/10/18 10:27:29 by sungjuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int	base_check(char *base)
+unsigned int	base_check(char *base)
 {
 	int	i;
 	int	j;
 
 	i = 0;
-	if (base[i + 1] == 0)
-		return (1);
 	while (base[i])
 	{
 		if (base[i] == '+' || base[i] == '-')
 		{
-			return (1);
+			return (0);
 		}
 		j = i + 1;
 		while (base[j])
 		{
 			if (base[i] == base[j])
-				return (1);
-				j++;
+				return (0);
+			j++;
 		}
 		i++;
 	}
-	return (0);
-}
-
-int	base_len(char *base)
-{
-	int	len;
-
-	len = 0;
-	while (base[len])
-	{
-		len++;
-	}
-	return (len);
+	if (i < 2)
+		return (0);
+	return (i);
 }
 
 void	ft_putnbr_base(int nbr, char *base)
 {
-	char			*b;
-	int				len;
+	unsigned int	len;
 	unsigned int	n;
-
-	b = base;
-	len = base_check (b);
-	if (len)
+	
+	len = base_check (base);
+	if (!len)
 		return ;
-	len = base_len (base);
 	if (nbr < 0)
 	{
-		n = -nbr;
+		n = nbr * -1;
 		write(1, "-", 1);
 	}
-	if (nbr > len)
-		ft_putnbr ((nbr / len), base);
-	write(1, base[nbr % len], 1);
+	else
+		n = nbr;
+	if (n > len)
+		ft_putnbr_base ((n / len), base);
+	write(1, base[n % len], 1);
 }
