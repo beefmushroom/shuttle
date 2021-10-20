51b631e4b72125fe3d034c02d75c816e55d6486c
phobeer <hamburgerisperfectfood@gmail.com>
2021-10-20 13:43:48 +0900

    06,05

--- C06/ex03/ft_sort_params.c
+++ C06/ex03/ft_sort_params.c
@@ -6,18 +6,18 @@
 /*   By: sungjuki <sungjuki@student.42seoul.kr      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 17:14:31 by sungjuki          #+#    #+#             */
-/*   Updated: 2021/10/20 12:06:58 by sungjuki         ###   ########.fr       */
+/*   Updated: 2021/10/20 13:39:52 by sungjuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int	strcmp(char *str1, char *str2)
+int	ft_strcmp(char *str1, char *str2)
 {
 	int	idx;
 
 	idx = 0;
-	while (str1[idx] == str2[idx])
+	while (str1[idx] && (str1[idx] == str2[idx]))
 	{
 		idx++;
 	}
@@ -26,46 +26,45 @@ int	strcmp(char *str1, char *str2)
 
 void	print(int argc, char **argv)
 {
-	char	buf;
 	int		i;
-	int 	j;
+	int		j;
 
 	i = 1;
-	while (i < argc - 1)
+	while (i < argc)
 	{
 		j = 0;
 		while (argv[i][j])
 		{
-			buf = argv[i][j] + '0';
-			write(1, &buf, 1);
-		}
-		i++;
-		write(1, " ", 1);
-	}
-}
-
-
-int main(int argc, char **argv)
-{
-	int	i;
-	int j;
-	char *tmp;
-
-	i = 1
-	while (i < argc - 1)
-	{
-		j = 0;
-		while (j < argc - 1 - i)
-		{
-			if (strcmp(argv[j], argv[j + 1]) > 0)
-			{
-				tmp = argv[j];
-				argv[j] = argv[j + 1];
-				argv[j + 1] = argv[j];
-			}
+			write(1, &argv[i][j], 1);
 			j++;
 		}
 		i++;
+		write(1, "\n", 1);
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int		j;
+	int		swap;
+	char	*tmp;
+
+	swap = 1;
+	while (swap)
+	{
+		swap = 0;
+		j = 1;
+		while (j < argc - 1)
+		{
+			if (ft_strcmp(argv[j], argv[j + 1]) > 0)
+			{
+				tmp = argv[j];
+				argv[j] = argv[j + 1];
+				argv[j + 1] = tmp;
+				swap = 1;
+			}
+			j++;
+		}
 	}
 	print(argc, argv);
 	return (0);
