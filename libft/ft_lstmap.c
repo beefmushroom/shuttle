bb684377b5d06572c81ad8914e7bf2a90da03786
Sungjun Kim <sungjuki@c6r4s1.42seoul.kr>
2021-12-22 13:55:15 +0900

    lst

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,11 +6,22 @@
 /*   By: sungjuki <sungjuki@student.42seoul.kr      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 16:21:44 by sungjuki          #+#    #+#             */
-/*   Updated: 2021/12/20 13:59:16 by sungjuki         ###   ########.fr       */
+/*   Updated: 2021/12/22 13:51:01 by sungjuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static t_list	**make_head_nod(void)
+{
+	t_list	**ans;
+
+	ans = (t_list **)malloc(sizeof(t_list *));
+	if (!ans)
+		return (0);
+	*ans = 0;
+	return (ans);
+}
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
@@ -19,8 +30,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 
 	if (!lst || !f)
 		return (0);
-	ans = (t_list **)malloc(sizeof(t_list *));
-	*ans = 0;
+	ans = make_head_nod();
 	while (lst)
 	{
 		tmp = ft_lstnew(f(lst->content));
--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: sungjuki <sungjuki@student.42seoul.kr      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 16:28:04 by sungjuki          #+#    #+#             */
-/*   Updated: 2021/12/21 17:25:02 by sungjuki         ###   ########.fr       */
+/*   Updated: 2021/12/22 13:17:04 by sungjuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ static char	*make_str(char *rst, char const *s1, size_t	tp1, size_t tp2)
 	size_t	idx;
 
 	idx = 0;
-	while (tp1 + idx <= tp2)
+	while (tp1 + idx < tp2)
 	{
 		rst[idx] = s1[tp1 + idx];
 		idx++;
@@ -47,17 +47,17 @@ char	*ft_strtrim(char const *s1, char const *set)
 	size_t	tp2;
 	size_t	idx;
 
-	if (!s1 || !set || !*s1)
+	if (!s1 || !set)
 		return ((char *)s1);
 	tp1 = 0;
 	while (s1[tp1] && is_set(s1[tp1], set))
 		tp1++;
-	tp2 = ft_strlen(s1) - 1;
+	tp2 = ft_strlen(s1);
 	idx = 0;
-	while (tp2 - idx > tp1 && is_set(s1[tp2 - idx], set))
+	while (tp2 - idx > tp1 + 1 && is_set(s1[tp2 - 1 - idx], set))
 		idx++;
 	tp2 = tp2 - idx;
-	rst = (char *)malloc(sizeof(char) * (tp2 - tp1 + 1) + 1);
+	rst = (char *)malloc(sizeof(char) * (tp2 - tp1) + 1);
 	if (!rst)
 		return (0);
 	rst = make_str(rst, s1, tp1, tp2);
