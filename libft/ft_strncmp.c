5706f27b441c036543743c6fe84d2d27a44b48ff
Sungjun Kim <sungjuki@c6r4s1.42seoul.kr>
2021-12-22 11:42:22 +0900

    norm

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: sungjuki <sungjuki@student.42seoul.kr      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/09 10:26:32 by sungjuki          #+#    #+#             */
-/*   Updated: 2021/12/16 18:24:26 by sungjuki         ###   ########.fr       */
+/*   Updated: 2021/12/22 11:31:30 by sungjuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 		return (0);
 	i = 0;
 	if (!ss1)
-		return (-*ss2);
+		return (- *ss2);
 	else if (!ss2)
 		return (*ss1);
 	while ((ss1[i] || ss2[i]) && i < n)
