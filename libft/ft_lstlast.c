37b9d0dfa46e882f369a8ede19c09166da5b06bd
phobeer <hamburgerisperfectfood@gmail.com>
2021-12-22 14:47:57 +0900

    final?

--- libft/ft_lstlast.c
+++ libft/ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: sungjuki <sungjuki@student.42seoul.kr      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 15:39:39 by sungjuki          #+#    #+#             */
-/*   Updated: 2021/12/20 13:06:03 by sungjuki         ###   ########.fr       */
+/*   Updated: 2021/12/22 13:56:49 by sungjuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,7 @@ t_list	*ft_lstlast(t_list *lst)
 {
 	if (!lst)
 		return (0);
-	while (1)
-	{
-		if (!lst->next)
-			break ;
+	while (lst->next)
 		lst = lst->next;
-	}
 	return (lst);
 }
